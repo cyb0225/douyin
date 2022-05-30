b45939bd335fbaee9e595c1e5e967e5bb9289af9
Sterben-01 <vocaloidmiku06@gmail.com>
2022-05-29 23:27:21 -0500

    update timestamp order

--- controller/videoctl/publish.go
+++ controller/videoctl/publish.go
@@ -2,13 +2,12 @@ package videoctl
 
 import (
 	"fmt"
+	"github.com/2103561941/douyin/controller/commonctl"
 	"github.com/2103561941/douyin/service/videosvc"
+	"github.com/gin-gonic/gin"
 	"net/http"
 	"path/filepath"
 	"time"
-
-	"github.com/2103561941/douyin/controller/commonctl"
-	"github.com/gin-gonic/gin"
 )
 
 func Publish(c *gin.Context) {
@@ -33,7 +32,7 @@ func Publish(c *gin.Context) {
 	user := commonctl.UserLoginMap[token]
 	title := c.PostForm("title")
 
-	finalName := fmt.Sprintf("%d_%s_%d", user.Id, filename, time.Now().Unix())
+	finalName := fmt.Sprintf("%d_%d_%s", user.Id, time.Now().Unix(), filename)
 	//需要判断同一用户上传同一个文件两次的情况。已修改。文件名后加unix时间戳
 	saveFile := filepath.Join("./video_content/", finalName)
 	if err := c.SaveUploadedFile(data, saveFile); err != nil {

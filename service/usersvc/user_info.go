cc1e5ac9b4886beddaed31362476a85b404bbbf3
Sterben-01 <vocaloidmiku06@gmail.com>
2022-06-11 23:36:57 -0500

    update avatar ..

--- service/usersvc/user_info.go
+++ service/usersvc/user_info.go
@@ -6,11 +6,14 @@ import (
 )
 
 type UserInfo struct {
-	Id            uint64 `json:"id"`
-	Name          string `json:"name"`
-	FollowCount   uint64 `json:"follow_count"`
-	FollowerCount uint64 `json:"follower_count"`
-	IsFollow      bool   `json:"is_follow"`
+	Id             uint64 `json:"id"`
+	Name           string `json:"name"`
+	FollowCount    uint64 `json:"follow_count"`
+	FollowerCount  uint64 `json:"follower_count"`
+	IsFollow       bool   `json:"is_follow"`
+	Signature      string `json:"signature"`
+	Avatar         string `json:"avatar"`
+	Background_img string `json:"background_image"`
 }
 
 func (user *UserInfo) getFollowStatus(id uint64) bool {
@@ -34,7 +37,7 @@ func (user *UserInfo) SetUserInfo(id uint64) error {
 	record := &repository.User{
 		Id: user.Id,
 	}
-	
+
 	if err := record.SelectByUserId(); err != nil {
 		return err
 	}
@@ -44,6 +47,9 @@ func (user *UserInfo) SetUserInfo(id uint64) error {
 	user.FollowCount = record.FollowCount
 	user.FollowerCount = record.FollowerCount
 	user.IsFollow = user.getFollowStatus(id)
+	user.Signature = "抖音青训营-源石技艺队-No.1107"
+	user.Avatar = "https://github.com/sterben-01/sterben-01.github.io/blob/main/assets/blog_res/2022-06-10-tempimage.assets/68458315_p0.png"
+	user.Background_img = "https://github.com/sterben-01/sterben-01.github.io/blob/main/assets/blog_res/2022-06-10-tempimage.assets/61163969_p0.jpg"
 
 	return nil
 }
